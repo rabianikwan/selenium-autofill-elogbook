c32382bc24d7c19eb4f8a9bfc22b31aa124be2ce
Rabian Ikwan <72639592+rabianikwan@users.noreply.github.com>
2023-06-09 13:03:55 +0000

    update

---
+++ controllers/handler.js
@@ -0,0 +1,12 @@
+let namaKegiatan = [];
+let uraianTugas = [
+    'Melakukan pengkajian keperawatan dasar pada individu',
+    'Melakukan komunikasi terapeutik dalam pemberian asuhan keperawatan',
+    'Memberikan oksigenasi sederhana',
+    'Melakukan intervensi keperawatan spesifik yang sederhana pada area medikal bedah',
+    'Melakukan dokumentasi tindakan keperawatan',
+    'Melakukan perawatan luka',
+    'Melakukan tindakan keperawatan dalam kondisi gawat darurat / bencana / kritikal'
+];
+let 
+const randomNumber = Math.trunc(Math.random() * 10) + 5;
