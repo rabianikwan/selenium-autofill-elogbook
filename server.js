9d59df490957f76e443abefe28ec6b8a79331d7e
Rabian Ikwan <rabianikwan67@gmail.com>
2023-07-02 23:09:27 +0800

    feat(#3): integration frontend and backend

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import * as path from "path";
 
 import * as url from 'url';
 
+import selenium from './utils/selenium.js'
+
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const port = 3000;
@@ -16,6 +18,10 @@ app.use(express.json())
 
 app.use(express.urlencoded({ extended : true}))
 
+const login = 'http://36.66.236.83/elogbook-samarinda/'
+
+const dashboard = 'http://36.66.236.83/elogbook-samarinda/index.php/aktivitas'
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 })
@@ -27,6 +33,8 @@ app.post('/', (req, res) => {
     const month = req.body.month
     const shiftScedule = req.body.shift
     console.log(nip, pass, month, shiftScedule)
+    // yang belum finished login, dashboard, fillFormatDate, editShift
+    selenium(login, nip, pass, dashboard, month, shiftScedule).then(() => console.log('Process finished'))
 
     res.status(201)
         .send('<h1>Selenium akan segera berjalan</h1>')
