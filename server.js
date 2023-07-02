ef905c3dd324e94dd95f84d6b46fb1d4d431f7cf
Rabian Ikwan <72639592+rabianikwan@users.noreply.github.com>
2023-07-02 14:13:30 +0800

    feat(#2): add react into project

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express')
 const app = express()
 const port = 4400
 const rootDir = require('./utils/path')
-const index = require('./route/index')
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(express.static('public'))
 
-app.use(index)
+app.use("/", (req, res) => {
+  res.status(200)
+      .sendFile(rootDir + '/views/index.html')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
